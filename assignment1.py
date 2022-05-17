354bb17269738c7b8624d923fce823179da6f025
TechMokami <joan.mokami@gmail.com>
2022-05-17 19:45:41 +0300

    assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -14,3 +14,25 @@ diameter = radius * 2
 pi = 3.142
 area_of_a_cylinder = int (pi) * int (radius) * int (radius) + int (pi) * int (diameter) * int (height)
 print("The area of the cylinder is " + str (area_of_a_cylinder))
+
+
+
+
+
+# Volume of a cylinder
+pi = 3.142
+radius = input("Enter radius of the circle ")
+height = input("Enter height of the cylinder ")
+volume = int (pi) * int (radius) * int (radius) * int (height)
+print("The volume of the cylinder = " + str (volume))
+
+
+
+
+
+# Volume of a cube
+length_of_cube = input("Enter length of cube ")
+width_of_cube = input("Enter width of the cube ")
+height_of_cube = input("Enter height of cube ")
+VolumeOfCube = int (length_of_cube) * int(width_of_cube) * int(height_of_cube)
+print("The volume of the cybe = " + str (VolumeOfCube))
