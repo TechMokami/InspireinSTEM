624f857c150d5f23f939696643e81abddf00d089
TechMokami <joan.mokami@gmail.com>
2022-05-23 09:11:22 +0300

    Lesson5

--- lesson5.py
+++ lesson5.py
@@ -1,24 +1,26 @@
 # String methods
 
-name = "Ada Lovelace"
-user_name = " Lovelace"
-user_name = "Lovelace "
-age = 18
-age = 45
-person = "I am "  + str (name) + " and I am  " + str(age) + " years old"
-print(person)
+# name = "Ada Lovelace"
+# user_name = " Lovelace"
+# user_name = "Lovelace "
+# age = 18
+# age = 45
+# person = "I am "  + str (name) + " and I am  " + str(age) + " years old"
+# print(person)
 
 
-# The format method
+# # The format method
 
-print("My name is {} and I am {} years old ".format(name, age)) 
-print (f"My name is {name} and I am {age} years old")
+# print("My name is {} and I am {} years old ".format(name, age)) 
+# print (f"My name is {name} and I am {age} years old")
 
-# Newline \n and tab \t
-print (f"My \tname is {name} \nand I am {age} years old")
+# # Newline \n and tab \t
+# print (f"My \tname is {name} \nand I am {age} years old")
 
-print(user_name.strip()) 
-print(name,age)
-print(user_name.lstrip())
-print(user_name.rstrip())
+# print(user_name.strip()) 
+# print(name,age)
+# print(user_name.lstrip())
+# print(user_name.rstrip())
+
+print("Monday\tTuesday\tWednesday\tThursday\tFriday\tSaturday\tSunday")
 
