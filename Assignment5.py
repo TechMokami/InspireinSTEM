bd7a7b84fd9e536ba1d331c2ee592358b27f4e6c
TechMokami <joan.mokami@gmail.com>
2022-05-25 09:35:54 +0300

    Using a for loop and if statement to access a single entity

---
+++ Assignment5.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# list of vehicles
+vehicles = ['bmw','audi','toyota','jeep','mercedees']
+# Using a for loop to print all vehicles
+# for vehicle in vehicles :
+#      if (vehicle == 'jeep') :
+#         print(vehicle.upper())
+for vehicle in vehicles :
+    print(vehicles[3])
+
