8734bd8468e4975f71a1228e4e7eec3568aae461
TechMokami <joan.mokami@gmail.com>
2022-05-26 00:10:11 +0300

    Introduction to If statements

---
+++ If_Statements.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+############################
+#       If Statements - Conditions
+#       Name: Joan Mokami
+#       Date: 24/05/2022
+############################
+
+# IF THEN ELSE...
+# age = 24
+# if age >= 18:
+#     print("You are allowed to drive")
+
+# else :
+#     print("You are underage")
+
+# age = 13
+# if age >= 18:
+#     print("You are allowed to drive")
+# else :
+#     print("You are underage")
