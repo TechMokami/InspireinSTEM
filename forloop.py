d8bcabb27c7b2400acaecedc1171811f481cc452
TechMokami <joan.mokami@gmail.com>
2022-05-27 08:31:07 +0300

    For Loops

---
+++ forloop.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+############################
+#      For Loops With Lists
+#       Name: Joan Mokami
+#       Date: 24/05/2022
+############################
+
+# squares = [] # Empty Lists
+# for number in range (0,10):
+#     square=number**2
+#     squares.append(square)
+    
+# print(squares)
+
+# cubes = []
+# for number in range (0,10):
+#     cube=number**3
+#     cubes.append(cube)
+# print(cubes)
+
+sum = 0
+for number in range (0,100):
+    sum=number+sum
+print(sum)
+
+
+
